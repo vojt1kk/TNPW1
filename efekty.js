22c80fce3db4e01592f8ecb309b37c75901b4dda
vojt1kk <kocourek.vojt1k@gmail.com>
2025-04-24 18:27:58 +0200

    commit-18

--- efekty.js
+++ efekty.js
@@ -20,3 +20,23 @@ dropdownLinks.addEventListener('mouseleave', () => {
 dropdownLinks.addEventListener('mouseenter', () => {
     dropdownLinks.classList.add('active');
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const typTisku = document.getElementById('typ-tisku');
+    const materialyFilament = document.getElementById('materialy-filament');
+    const materialyResin = document.getElementById('materialy-resin');
+
+    function updateMaterialSelect() {
+      if (typTisku.value === 'filament') {
+        materialyFilament.classList.remove('hidden');
+        materialyResin.classList.add('hidden');
+      } else {
+        materialyFilament.classList.add('hidden');
+        materialyResin.classList.remove('hidden');
+      }
+    }
+
+    typTisku.addEventListener('change', updateMaterialSelect);
+    updateMaterialSelect(); // Spustí kontrolu při načtení stránky
+  });
